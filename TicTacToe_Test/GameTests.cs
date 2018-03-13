9c2bb4eec137e6a5c94cc827ca26814337c123c2
Luc A Pergolotti <tuf48949@temple.edu>
2018-03-13 18:40:06 -0400

    Finish lab 6

---
+++ TicTacToe_Test/GameTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tictactoe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tictactoe.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void ActiveFields()
+        {
+            Game game = new Game();
+            game.CreateBoard();
+
+            Assert.AreEqual(9, game.board.Length);
+        }
+
+        [TestMethod()]
+        public void CurrentPlayerIsX()
+        {
+            Game game = new Game();
+            
+            Assert.AreEqual(1, game.currentPlayer);
+        }
+
+        [TestMethod()]
+        public void CurrentPlayerIsO()
+        {
+            Game game = new Game();
+            game.ChangePlayer();
+            Assert.AreEqual(2, game.currentPlayer);
+        }
+
+        [TestMethod()]
+        public void MakeMoveTestX()
+        {
+            Game game = new Game();
+            game.CreateBoard();
+            game.MakeMove(0, 1);
+            Assert.AreEqual(1,game.board[0,1]);
+        }
+
+        [TestMethod()]
+        public void MakeMoveTestO()
+        {
+            Game game = new Game();
+            game.CreateBoard();
+            game.ChangePlayer();
+            game.MakeMove(0, 2);
+            Assert.AreEqual(2, game.board[0, 2]);
+        }
+
+        [TestMethod()]
+        public void SpaceInUseTest()
+        {
+            Game game = new Game();
+            game.CreateBoard();
+
+            Assert.AreEqual(false, game.SpaceInUse(0, 0));
+        }
+    }
+}
---
+++ Tictactoe/Game.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tictactoe
+{
+    public class Game
+    {
+        public int[,] board;
+        public int currentPlayer = 1;
+        
+        public void CreateBoard()
+        {
+            board = new int[3, 3];
+            for(int i = 0; i > 3; i++)
+            {
+                for(int j = 0; j>3; j++)
+                {
+                    board[i, j] = 0;
+                }
+            }
+        }
+
+        public int CurrentPlayer()
+        {
+            return currentPlayer;
+        }
+
+        public void ChangePlayer()
+        {
+            if(currentPlayer == 1)
+            {
+                currentPlayer = 2;
+            }
+            else if (currentPlayer == 2)
+            {
+                currentPlayer = 1;
+            }
+        }
+
+        public void MakeMove(int x, int y)
+        {
+            if (!SpaceInUse(x,y))
+            {
+                board[x, y] = currentPlayer;
+            }
+            
+        }
+
+        public bool SpaceInUse(int x, int y)
+        {
+            if (board[x,y] == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
